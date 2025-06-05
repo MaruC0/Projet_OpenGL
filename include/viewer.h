052317f6d6609a3a2a1850ec804c905fbd867e51
BudeureounGureum <matheo.bouiller@protonmail.com>
2025-06-05 23:58:08 +0200

    Ajout du sol

--- include/viewer.h
+++ include/viewer.h
@@ -11,6 +11,10 @@
 #include "shader.h"
 #include "node.h"
 #include "skybox.h"
+#include "ground.h"
+#include "texture.h"
+#include "camera.h"
+#include "model.h"
 
 class Viewer {
 public:
@@ -34,6 +38,8 @@ private:
     static void key_callback_static(GLFWwindow* window, int key, int scancode, int action, int mods);
     Shader* skybox_shader;
     Skybox* skybox;
+    Ground* ground;
+    glm::mat4 ground_mat;
 };
 
 #endif // VIEWER_H
