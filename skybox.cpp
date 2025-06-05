6a86330de8a35e9b5ebbdcb1ca2c7721afedfbc0
BudeureounGureum <matheo.bouiller@protonmail.com>
2025-06-05 04:00:24 +0200

    Skybox opérationnelle

--- skybox.cpp
+++ skybox.cpp
@@ -1,6 +1,8 @@
 #include "skybox.h"
-#include "stb_image.h"
 #include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 
 
 unsigned int Skybox::loadCubemap(std::vector<std::string> faces)
@@ -16,7 +18,7 @@ unsigned int Skybox::loadCubemap(std::vector<std::string> faces)
         if (data)
         {
             glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
-                0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+                0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data
             );
             stbi_image_free(data);
         }
@@ -26,6 +28,7 @@ unsigned int Skybox::loadCubemap(std::vector<std::string> faces)
             stbi_image_free(data);
         }
     }
+
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -71,53 +74,46 @@ Skybox::Skybox(Shader* shader_program, std::vector<std::string> faces) : shader_
         1, 2, 5
     };
     
-    cubemapTexture = 0;
+    cubemapTexture = loadCubemap(textures);;
 
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
-    // Vertex buffer
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+    glGenBuffers(2, &buffers[0]);
 
-    // Index buffer
-    glGenVertexArrays(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // Vertex array object
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), NULL);
     glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[1]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 }
 
 void Skybox::draw(glm::mat4& model, glm::mat4& view, glm::mat4& projection)
 {
-    cubemapTexture = loadCubemap(textures);
-    glDepthMask(GL_FALSE);
-    glUseProgram(shader_program_);
-    glBindVertexArray(VAO);
+    glDepthFunc(GL_EQUAL);
+    glUseProgram(this->shader_program_);
     glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+    glBindVertexArray(VAO);
     
-    /*GLint loc = glGetUniformLocation(shader_program_, "model");
-    glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(model));
-
-    loc = glGetUniformLocation(shader_program_, "view");
-    glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(view));
+    // Permet d'être "dans" la skybox
+    glm::mat4 view_no_translation = glm::mat4(glm::mat3(view));
+    GLint loc = glGetUniformLocation(shader_program_, "view");
+    glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(view_no_translation));
 
     loc = glGetUniformLocation(shader_program_, "projection");
-    glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(projection));*/
+    glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(projection));
 
     glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-    glDepthMask(GL_TRUE);
+    glDepthFunc(GL_LESS);
 }
 
 Skybox::~Skybox() {
     glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
-    glDeleteTextures(6, &cubemapTexture);
+    glDeleteBuffers(1, &buffers[0]);
+    glDeleteTextures(1, &cubemapTexture);
 }
 
 void Skybox::key_handler(int key) {
